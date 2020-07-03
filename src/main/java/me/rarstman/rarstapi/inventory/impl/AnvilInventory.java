d6b6acca402f0268bb15ab13448e8b7d5507afd2
RarstManPL <bobiczek205@interia.pl>
2020-07-03 21:22:23 +0200

    AnvilInventory fixes

--- src/main/java/me/rarstman/rarstapi/inventory/impl/AnvilInventory.java
+++ src/main/java/me/rarstman/rarstapi/inventory/impl/AnvilInventory.java
@@ -2,14 +2,14 @@ package me.rarstman.rarstapi.inventory.impl;
 
 import me.rarstman.rarstapi.RarstAPIPlugin;
 import me.rarstman.rarstapi.inventory.InventoryProvider;
-import me.rarstman.rarstapi.task.impl.LaterTask;
 import net.wesjd.anvilgui.AnvilGUI;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
-public class AnvilInventory extends InventoryProvider<BiFunction<Player, String, String>, Consumer<Player>, AnvilInventory> {
+public class AnvilInventory extends InventoryProvider<BiFunction<Player, String, String>, Consumer<InventoryCloseEvent>, AnvilInventory> {
 
     private AnvilGUI.Builder anvilGUI;
     private final String text;
@@ -33,10 +33,13 @@ public class AnvilInventory extends InventoryProvider<BiFunction<Player, String,
         this.anvilGUI = new AnvilGUI.Builder()
                 .plugin(RarstAPIPlugin.getAPI())
                 .title(this.title)
-                .item(this.clickableItems.get(0) == null ? this.clickableItems.get(0).getItemStack() : null)
-                .onClose(this.onClose)
                 .onComplete(this.onComplete)
                 .text(this.text);
+        this.clickableItems
+                .entrySet()
+                .stream()
+                .filter(entrySet -> entrySet.getKey() < 3)
+                .forEach(entrySet -> this.anvilGUI.setItem(entrySet.getKey(), entrySet.getValue().getItemStack()));
         return this;
     }
 
